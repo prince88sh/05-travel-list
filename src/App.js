43a8aed92f6aeb62f03f8f737326d3a44e706cf9
PRINCE SHARMA <prince.sanjay_sharma@nokia.com>
2024-02-13 00:57:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      HELLO REACT LETS START BUILDING TRAVEL LIST WEBSITE'S
+    </div>
+  );
+}
+
+export default App;
