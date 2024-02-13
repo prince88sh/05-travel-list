aa6ec8dbd61d7c602612b8921f5fd9c79bbdf229
PRINCE SHARMA <prince159sh@gmail.com>
2024-02-13 10:16:20 +0530

    forming layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import "./App.css";
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Brush", quantity: 1, packed: true },
+  { id: 4, description: "Water Bottle", quantity: 4, packed: true },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      HELLO REACT LETS START BUILDING TRAVEL LIST WEBSITE'S
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have x items in the list, you are already packed X</em>
+    </footer>
+  );
+}
